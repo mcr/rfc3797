940ff587e5997c8bc2538b245e538ecf31312c51
Harald Alvestrand <hta@chromium.org>
2015-07-05 05:51:21 +0200

    Made tests independent of having . in $PATH

--- testcase-2012.sh
+++ testcase-2012.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p OUTPUT
-rfc3797 <<EOF | tee OUTPUT/output-2012.txt | diff - output-2012.txt
+./rfc3797 <<EOF | tee OUTPUT/output-2012.txt | diff - output-2012.txt
 134
 12
 17 20 22 35 38 48 36
--- testcase-2013.sh
+++ testcase-2013.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p OUTPUT
-rfc3797 <<EOF| tee OUTPUT/output-2013.txt | diff - output-2013.txt
+./rfc3797 <<EOF| tee OUTPUT/output-2013.txt | diff - output-2013.txt
 154
 12
 7 8 11 17 19 25 46
